aed4f9c77782f2ac08b88da32b373eb1f62e447f
Raj kumar <rajk76986@gmail.com>
2025-04-04 13:58:18 +0530

    Update ayushman.js

--- ayushman.js
+++ ayushman.js
@@ -1,5 +1,5 @@
 // Replace with your MapTiler API key
-const mapTilerApiKey = 'mbfz4Kf5w1KFmN1gZefd';
+const mapTilerApiKey = 'MY_API_KEY_HERE';
 
 // Mock data for Ayushman Kendra centers
 const mockCenters = [
